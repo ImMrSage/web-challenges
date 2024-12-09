ccfc8c4d44dc1820c4defac2dc48e6cb91af891c
Sergii <96418850+ImMrSage@users.noreply.github.com>
2024-12-09 15:51:43 +0100

    make the button uppercase the input

--- javascript/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -2,3 +2,9 @@ console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
 const buttonUppsercase = document.querySelector('[data-js="button-uppercase"]');
+
+buttonUppsercase.addEventListener("click", () => {
+  const textInput = firstInput.value;
+  const uppercasedText = textInput.toUpperCase();
+  firstInput.value = uppercasedText;
+});
