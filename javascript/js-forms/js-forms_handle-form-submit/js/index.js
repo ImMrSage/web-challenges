158e8eb9db7f60d711d8be1ac73961917c20609e
Sergii <96418850+ImMrSage@users.noreply.github.com>
2024-12-10 11:47:49 +0100

    create form data and log into the console

--- javascript/js-forms/js-forms_handle-form-submit/js/index.js
+++ javascript/js-forms/js-forms_handle-form-submit/js/index.js
@@ -4,4 +4,8 @@ const form = document.querySelector('[data-js="form"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
 });
