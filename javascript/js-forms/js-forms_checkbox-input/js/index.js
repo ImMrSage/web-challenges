ca415bb6ffd8c559d0604c805ca6cfa08cff30fc
Sergii <96418850+ImMrSage@users.noreply.github.com>
2024-12-10 14:05:46 +0100

    make checkbox tos work as intended

--- javascript/js-forms/js-forms_checkbox-input/js/index.js
+++ javascript/js-forms/js-forms_checkbox-input/js/index.js
@@ -14,9 +14,13 @@ function showTosError() {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+  console.log(tosCheckbox.checked);
   // --v-- write your code here --v--
-
+  if (tosCheckbox.checked === true) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
