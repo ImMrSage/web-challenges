71c8d40f6769104f6c2482149407c1f5ee8b17eb
Sergii <96418850+ImMrSage@users.noreply.github.com>
2024-12-16 16:45:26 +0100

    fetch Darth Vader and R2D2 eye color

--- javascript/js-fetch/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -4,7 +4,12 @@ const url = "https://swapi.py4e.com/api/people";
 
 async function fetchData() {
   const response = await fetch(url);
-  console.log(response);
+  const data = await response.json();
+  const darthVader = data.results[3];
+  const r2d2 = data.results[2].eye_color;
+  console.log(data);
+  console.log(darthVader);
+  console.log(`R2-D2 eye color: ${r2d2}`);
 }
 
 fetchData();
