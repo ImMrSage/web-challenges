6896a784413f5393043bb4f73edb0d9f4fa2ce09
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-03 00:00:39 +0100

    change the output background color

--- javascript/js-functions/js-functions_pizza-analyzer/js/index.js
+++ javascript/js-functions/js-functions_pizza-analyzer/js/index.js
@@ -12,6 +12,7 @@ pizzaInput1.addEventListener("input", () => {
   const pizzaSize2 = Number(pizzaInput2.value);
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
@@ -19,6 +20,7 @@ pizzaInput2.addEventListener("input", () => {
   const pizzaSize2 = Number(pizzaInput2.value);
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
@@ -40,3 +42,8 @@ function updatePizzaDisplay(pizzaElement, newSize) {
 
 // Task 3
 // define the function updateOutputColor here
+function updateOutputColor(size1, size2) {
+  outputSection.style.backgroundColor = `var(${
+    size1 <= size2 ? "--green" : "--red"
+  })`;
+}
