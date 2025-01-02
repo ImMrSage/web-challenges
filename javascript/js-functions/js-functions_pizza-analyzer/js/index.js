27d08e96a9c5f35b34537e9b379f7aa976f98a0f
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-02 23:08:08 +0100

    change pizza display size with input

--- javascript/js-functions/js-functions_pizza-analyzer/js/index.js
+++ javascript/js-functions/js-functions_pizza-analyzer/js/index.js
@@ -5,26 +5,25 @@ const pizza1 = document.querySelector('[data-js="pizza-1"]');
 const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
 const pizza2 = document.querySelector('[data-js="pizza-2"]');
 const outputSection = document.querySelector('[data-js="output-section"]');
-
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
   const pizzaSize1 = Number(pizzaInput1.value);
   const pizzaSize2 = Number(pizzaInput2.value);
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
 });
 
 pizzaInput2.addEventListener("input", () => {
   const pizzaSize1 = Number(pizzaInput1.value);
   const pizzaSize2 = Number(pizzaInput2.value);
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
 function calculatePizzaGain(diameter1, diameter2) {
-  // diameter1 = pizzaInput1.value;
-  // diameter2 = pizzaInput2.value;
   pizza1Area = (diameter1 ** 2 * Math.PI) / 4;
   pizza2Area = (diameter2 ** 2 * Math.PI) / 4;
   pizzaGain = ((pizza2Area - pizza1Area) / pizza1Area) * 100;
@@ -33,6 +32,11 @@ function calculatePizzaGain(diameter1, diameter2) {
 
 // Task 2
 // define the function updatePizzaDisplay here
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const updateSize = (newSize / 24) * 100;
+  pizzaElement.style.width = `${updateSize}px`;
+  console.log(`Updating pizza display: ${newSize}px`);
+}
 
 // Task 3
 // define the function updateOutputColor here
