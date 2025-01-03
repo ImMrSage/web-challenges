cc8a67a21de243bf7fb51926a54b1738ba0d9749
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-03 17:02:43 +0100

    prepare querySelectors

--- javascript/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,5 @@
 console.clear();
+
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
