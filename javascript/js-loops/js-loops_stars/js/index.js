7b4120a78b8bfaedc3da22215fb1b9bb30a27563
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-05 16:54:00 +0100

    add empty stars loop

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -7,7 +7,10 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (let i = 1; i <= 5; i++) {
+    let star = "<img src='assets/star-empty.svg' alt='assets/star-empty.svg'>";
+    starContainer.innerHTML += star;
+  }
   // --^-- write or modify code above this line --^--
 }
 
