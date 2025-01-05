8e88a4cdb83254153ba581f90f84c53dc70607e6
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-05 17:15:34 +0100

    change number of filled stars

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -2,16 +2,18 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
   for (let i = 1; i <= 5; i++) {
-    let star = "<img src='assets/star-empty.svg' alt='assets/star-empty.svg'>";
+    const star = `<img src='assets/${
+      i <= filledStars ? "star-filled" : "star-empty"
+    }.svg' alt='assets/star-empty.svg'>`;
     starContainer.innerHTML += star;
   }
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(2);
