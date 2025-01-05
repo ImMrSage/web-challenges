585afdfb32902e3a172cc258666fa7abef791338
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-05 22:17:07 +0100

    create a second post through create element

--- javascript/js-create-element/js-createelement_social-media-post/js/index.js
+++ javascript/js-create-element/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,29 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+//Article
+const post = document.createElement("article");
+document.body.append(post);
+post.classList.add("post");
+//p content
+const postContent = document.createElement("p");
+post.append(postContent);
+postContent.classList.add("post__content");
+postContent.textContent = "It`s a social media post. More content is coming.";
+//footer
+const footer = document.createElement("footer");
+post.append(footer);
+footer.classList.add("post__footer");
+//span username
+const username = document.createElement("span");
+footer.append(username);
+username.classList.add("post__username");
+username.textContent = "@username2";
+//like button
+const button = document.createElement("button");
+footer.append(button);
+button.setAttribute("type", "button");
+button.classList.add("post__button");
+button.textContent = "♥ Like";
+button.addEventListener("click", handleLikeButtonClick);
