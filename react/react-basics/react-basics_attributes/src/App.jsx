b52bd9e2627d6e0649370e088e94405efcc58191
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-06 16:05:08 +0100

    create an article with attributes

--- react/react-basics/react-basics_attributes/src/App.jsx
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Let`s talk about games</h2>
+      <label htmlFor="input">What`s your favorite game franchise?</label>
+      <input type="text" id="input" />
+      <a href="https://en.wikipedia.org/wiki/Heroes_of_Might_and_Magic">
+        It`s a link to my favorite game franchise
+      </a>
+    </article>
+  );
 }
