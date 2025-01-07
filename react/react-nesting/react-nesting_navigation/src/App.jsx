5f8a45c925a7de1865f986ba2bdf382c35705e38
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-07 16:14:15 +0100

    replace and restructure logo and avatar

--- react/react-nesting/react-nesting_navigation/src/App.jsx
+++ react/react-nesting/react-nesting_navigation/src/App.jsx
@@ -2,18 +2,14 @@ import "./styles.css";
 import Header from "./components/Header";
 import { Navigation } from "./components/Nav";
 import { Link } from "./components/Link";
-import Image from "./components/Image";
-
-import avatar from "./img/avatar.jpg";
-import logo from "./img/logo.jpg";
+import Logo from "./components/Logo";
+import Avatar from "./components/Avatar";
 
 export default function App() {
   return (
     <>
       <Header className="header">
-        <Link href="#">
-          <Image className="round-image" src={logo} alt="logo" />
-        </Link>
+        <Logo />
         <Navigation>
           <Link className="navigation__link" href="#home">
             Home
@@ -25,13 +21,7 @@ export default function App() {
             Impressum
           </Link>
         </Navigation>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <Image className="round-image" src={avatar} alt="avatar" />
-        </button>
+        <Avatar />
       </Header>
       <main>content goes here…</main>
     </>
