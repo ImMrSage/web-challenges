e16fa81e9535156653d39b23125725d24db475a6
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-09 21:51:48 +0100

    restrict negative numbers

--- react/react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react/react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   }
 
   function handleDelete() {
-    setPeople(people - 1);
+    setPeople(people > 0 ? people - 1 : 0);
   }
   return (
     <div className="container">
