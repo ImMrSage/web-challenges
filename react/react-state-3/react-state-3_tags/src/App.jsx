7bf0d5a2bf6c6de24b1a54a31225c560681616dc
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-13 09:23:34 +0100

    create add tag function

--- react/react-state-3/react-state-3_tags/src/App.jsx
+++ react/react-state-3/react-state-3_tags/src/App.jsx
@@ -6,9 +6,13 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddtag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
   return (
     <main className="app">
-      <Form />
+      <Form onAddTag={handleAddtag} />
       <List tags={tags} />
     </main>
   );
