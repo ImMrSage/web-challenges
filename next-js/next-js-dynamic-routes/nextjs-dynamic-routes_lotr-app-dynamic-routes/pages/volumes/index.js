5aa285a70ab2e6c4ac614eef027adc9ac76fac6e
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-17 13:04:35 +0100

    create a random volume button

--- next-js/next-js-dynamic-routes/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ next-js/next-js-dynamic-routes/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import { introduction, volumes } from "../../lib/data";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
+  const router = useRouter();
+  function handleRandomRedirect() {
+    const randomIndex = Math.floor(Math.random() * volumes.length);
+    const randomVolume = volumes[randomIndex];
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
   return (
     <>
       <h1>The Lord of the Rings</h1>
@@ -13,6 +20,9 @@ export default function Volumes() {
           </li>
         ))}
       </ul>
+      <button type="button" onClick={handleRandomRedirect}>
+        Random Volume
+      </button>
     </>
   );
 }
