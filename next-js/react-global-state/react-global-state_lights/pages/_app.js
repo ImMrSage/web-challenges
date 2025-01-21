8f4f6f3b9b29400a381a5c5941da030ecff1a20b
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-01-21 12:40:17 +0100

    lift light state to the app and map through rooms

--- next-js/react-global-state/react-global-state_lights/pages/_app.js
+++ next-js/react-global-state/react-global-state_lights/pages/_app.js
@@ -1,11 +1,65 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialRooms = [
+  {
+    name: "Living Room",
+    isOn: false,
+    id: 1,
+  },
+  {
+    name: "Kitchen",
+    isOn: false,
+    id: 2,
+  },
+  {
+    name: "Bedroom",
+    isOn: false,
+    id: 3,
+  },
+  {
+    name: "Bathroom",
+    isOn: false,
+    id: 4,
+  },
+  {
+    name: "Garage",
+    isOn: false,
+    id: 5,
+  },
+  {
+    name: "Porch",
+    isOn: false,
+    id: 6,
+  },
+  {
+    name: "Garden",
+    isOn: false,
+    id: 7,
+  },
+  {
+    name: "Office",
+    isOn: false,
+    id: 8,
+  },
+];
 
 export default function App({ Component, pageProps }) {
+  const [rooms, setRooms] = useState(initialRooms);
+
+  function handleToggle(roomId) {
+    setRooms(
+      rooms.map((room) =>
+        room.id === roomId ? { ...room, isOn: !room.isOn } : room
+      )
+    );
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} handleToggle={handleToggle} isOn={isOn} />
     </Layout>
   );
 }
